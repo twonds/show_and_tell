63750501d4ff61f6e1f1c4c04242d49fa6ba68df
Christopher Zorn <christopherzo@zillowgroup.com>
2019-09-09 18:00:18 -0700

    basic setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='show_and_tell',
+      version='1.0',
+      description='Interactive command line demos',
+      author='Christopher Zorn',
+      author_email='christopher.zorn@gmail.com',
+      packages=['show_and_tell'],
+     )
